329eb59eb4900fb4ddc0bbc936fef8f6647661b2
LÂM HOÀNG SANG <lamhoangsang169@gmail.com>
2026-09-18 01:32:37 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,10 +12,8 @@ def compress_image_to_base64(uploaded_file, max_size=(800, 800), quality=70):
     try:
         image = Image.open(uploaded_file)
         image.thumbnail(max_size)
-        
         if image.mode in ("RGBA", "P"):
             image = image.convert("RGB")
-            
         buffered = io.BytesIO()
         image.save(buffered, format="JPEG", quality=quality)
         return base64.b64encode(buffered.getvalue()).decode()
@@ -28,10 +26,8 @@ def calculate_exact_minutes(start_date_str, start_time_str, end_date_str, end_ti
     try:
         dt_start_str = f"{start_date_str} {start_time_str}"
         dt_end_str = f"{end_date_str} {end_time_str}"
-        
         dt_start = datetime.datetime.strptime(dt_start_str, "%Y-%m-%d %H:%M:%S")
         dt_end = datetime.datetime.strptime(dt_end_str, "%Y-%m-%d %H:%M:%S")
-        
         diff = dt_end - dt_start
         total_minutes = int(diff.total_seconds() / 60)
         return max(0, total_minutes)
