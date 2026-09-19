ce432b1f6e0a53a373d0bf40a16551e3a532b64d
LÂM HOÀNG SANG <lamhoangsang169@gmail.com>
2026-09-19 15:37:45 +0700

    Update __init__.py

--- view/__init__.py
+++ view/__init__.py
@@ -1,6 +1,2 @@
-from .nhap_san_luong import render_nhap_san_luong
-from .cham_cong import render_cham_cong
-from .bao_cao import render_bao_cao
-from .thu_muc_bao_cao import render_thu_muc_bao_cao
-from .dinh_muc_cong_viec import render_dinh_muc_cong_viec
-from .thung_rac import render_thung_rac
+# view/__init__.py
+# File đánh dấu package view
